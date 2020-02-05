1dcc94c3ecd67fb5d2ee674daa5ea36febac1f42
lgblkb <dbakhtiyarov@nu.edu.kz>
2020-02-05 19:04:20 +0600

    Commit from Dias

--- app/worker/worker_conf.py
+++ app/worker/worker_conf.py
@@ -3,5 +3,5 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-app = Celery('tasks', backend='rpc://', broker='pyamqp://')
+app = Celery('tasks', backend='rpc://')
 app.conf.update(settings.CELERY)
